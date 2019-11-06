9a781e5c70a92cf3a5db6ec7423a9f825a9d9ac2
Noach Nathanael T <noach.tjahjadi@gmail.com>
2019-11-06 20:05:54 +0700

    Meeting08 - Movie Tracker

---
+++ Meeting08 - Movie Tracker/app/src/main/java/com/github/noachnt/movietracker/moviedetails/MovieDetailsFragment.kt
@@ -0,0 +1,57 @@
+package com.github.noachnt.movietracker.moviedetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import com.github.noachnt.movietracker.R
+import com.github.noachnt.movietracker.database.MovieDatabase
+import com.github.noachnt.movietracker.databinding.FragmentMovieDetailsBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MovieDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentMovieDetailsBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_movie_details, container, false)
+
+        val application = requireNotNull(this.activity).application
+        val arguments = MovieDetailsFragmentArgs.fromBundle(arguments!!)
+        val dataSource = MovieDatabase.getInstance(application).movieDatabaseDao
+
+        val viewModelFactory = MovieDetailsViewModelFactory(arguments.movieKey, dataSource)
+        val movieDetailsViewModel = ViewModelProviders.of(this, viewModelFactory)
+                                        .get(MovieDetailsViewModel::class.java)
+
+        binding.movieDetailsViewModel = movieDetailsViewModel
+
+        movieDetailsViewModel.navigateToMovieTracker.observe(this, Observer {status ->
+            when (status) {
+                "clear" -> {
+                    this.findNavController().navigate(MovieDetailsFragmentDirections
+                        .actionMovieDetailsFragmentToMovieTrackerFragment())
+                    movieDetailsViewModel.doneNavigating()
+                }
+                "emptyMovieName" -> Toast.makeText(this.context, "NEED TITLE.", Toast.LENGTH_SHORT).show()
+                "emptyRating" -> Toast.makeText(this.context, "RATE THE MOVIE FIRST PLS.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
---
+++ Meeting08 - Movie Tracker/app/src/main/java/com/github/noachnt/movietracker/movietracker/MovieTrackerViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.github.noachnt.movietracker.movietracker
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.noachnt.movietracker.database.MovieDatabaseDao
+
+class MovieTrackerViewModelFactory (
+    private val dataSource: MovieDatabaseDao,
+    private val application: Application) : ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override  fun <T : ViewModel?> create (modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MovieTrackerViewModel::class.java)) {
+            return MovieTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalAccessException("ViewModel class not recognized.")
+    }
+
+}
